90d581040fe5313c519afcd2bbfd6522b9d8157d
Phan The Dung <ptdung13@gmail.com>
2010-10-19 11:45:53 +0900

    done with routing

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -19,8 +19,10 @@ class jobActions extends sfActions
 
   public function executeShow(sfWebRequest $request)
   {
-  $this->job = Doctrine::getTable('JobeetJob')-> find($request->getParameter('id'));
-  $this->forward404Unless($this->job);
+    $this->job = $this->getRoute()->getObject();
+
+    $this->forward404Unless($this->job);
+
 
   }
 
